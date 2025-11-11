d4a7eaf76ce70ba8817c97d7fc66db5c8601bd16
Victoria <vic@MacBook-Air.local>
2025-11-11 09:42:53 +0100

    feat: añadida estructura base de rutas y componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { TripList } from './pages/trip-list/trip-list';
+import { TripDetail } from './pages/trip-detail/trip-detail';
+import { UserProfile } from './pages/user-profile/user-profile';
+
+export const routes: Routes = [
+    { path: '', component: Home }, 
+  { path: 'login', component: Login }, 
+  { path: 'registro', component: Register }, 
+  { path: 'viajes', component: TripList }, 
+  { path: 'viajes/:id', component: TripDetail }, 
+  { path: 'perfil', component: UserProfile },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/pages/trip-detail/trip-detail.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './trip-detail.html',
+  styleUrl: './trip-detail.css',
+})
+export class TripDetail {
+
+}
